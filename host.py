53dc285157500b5b3439f53c22c9b4749a1181a9
ralph-hatoum <hatoumralph@gmail.com>
2024-02-23 19:43:24 +0100

    functions to upload images to ipfs from paths

--- host.py
+++ host.py
@@ -3,6 +3,7 @@ from moduleImport import importPackage
 importPackage()
 
 import requests
+import os
 
 # IPFS API endpoint
 api_url = 'http://127.0.0.1:5001/api/v0/add'
@@ -24,9 +25,32 @@ def get_cid_from_daemon_response(response):
 def upload_images_to_ipfs(images_paths, api_url):
     CIDs = {}
     for image in images_paths:
-        response = upload_images_to_ipfs(image, api_url)
+        response = upload_file_to_ipfs(image, api_url)
         if response != None:
             CIDs[image]=get_cid_from_daemon_response(response)
 
     return CIDs
+
+def get_all_paths(directory):
+    """
+    Get paths to all elements (files and subdirectories) in the specified directory.
     
+    Args:
+    - directory (str): The directory to search for elements.
+    
+    Returns:
+    - List[str]: A list of paths to all elements found.
+    """
+    all_paths = []
+
+    for filename in os.listdir(directory):
+        full_path = os.path.join(directory, filename)
+        all_paths.append(full_path)
+
+    return all_paths
+
+images = get_all_paths("images")
+
+CIDs = upload_images_to_ipfs(images, api_url)
+
+print(CIDs)
