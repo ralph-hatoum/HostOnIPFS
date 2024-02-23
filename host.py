c7e27e61e09f0a21c96c86ee4629e84f2408df05
ralph-hatoum <hatoumralph@gmail.com>
2024-02-23 20:06:32 +0100

    comments

--- host.py
+++ host.py
@@ -10,6 +10,16 @@ api_url = 'http://127.0.0.1:5001/api/v0/add'
 
 
 def upload_file_to_ipfs(file_path, api_url):
+    """
+    Upload file to IPFS
+
+    Args:
+    - file_path (str) : path to file to upload
+    - api_url (str): URL to the IPFS daemon.
+
+    Returns:
+    - dict : response from server OR None : in case upload is not succesful
+    """
     files = {'file': open(file_path, 'rb')}
     response = requests.post(api_url, files=files)
     if response.status_code == 200:
@@ -20,9 +30,28 @@ def upload_file_to_ipfs(file_path, api_url):
         return None
     
 def get_cid_from_daemon_response(response):
+    """
+    Get CID from the daemon's response
+
+    Args:
+    - response (dict): response from daemon
+
+    Returns:
+    - str : CID of file uploaded
+    """
     return response['Hash']
 
 def upload_images_to_ipfs(images_paths, api_url):
+    """
+    Upload images to ipfs 
+    
+    Args:
+    - image_paths (List[str]): List of paths to images.
+    - api_url (str): URL to the IPFS daemon.
+    
+    Returns:
+    - Dict: Dictionary of CIDs of files upload (keys are the images' paths).
+    """
     CIDs = {}
     for image in images_paths:
         response = upload_file_to_ipfs(image, api_url)
